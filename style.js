e2daea40c3b6ba25d1d415dfae2f86dcd557d5db
Christian <criss.reeche@gmail.com>
2022-05-30 12:02:08 +0200

    progress

---
+++ style.js
@@ -0,0 +1,16 @@
+// ---- Meny ---- //
+var navLinks = document.getElementById("navLinks");
+    
+function visaMeny(){
+    navLinks.style.right = "0";
+}
+function doljMeny(){
+    navLinks.style.right = "-200px";
+}
+
+// ---- Bilder Zoom - scale down ---- //
+
+// https://code-boxx.com/image-zoom-css-javascript/ 
+
+
+
